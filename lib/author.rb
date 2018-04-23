2a31d370fd317e6c6f1486a81655d745763df299
Michael Boroski <mjboroski@gmail.com>
2018-04-23 22:45:56 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,7 @@
 class Author
 
   attr_accessor :name,:posts
+  @@song_count=0
 
   def initialize(name)
     @name=name
@@ -8,7 +9,19 @@ class Author
   end
 
   def add_post(post)
-
+    @posts<<post
+    post.artist=self
+    self.post_count += 1
   end
+
+  def add_post_by_title(post_title)
+    song= Post.new("#{post_title}")
+    add_post(song)
+  end
+
+  def song_count
+    self.song_count
+  end
+
 
 end
